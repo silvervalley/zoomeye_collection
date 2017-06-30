0cff4c3099524823a1cb6ec269e04faadb7a9414
thomas <youngtala@gmail.com>
2017-06-30 21:47:02 +0800

    update zoomeye for get ip address

--- zoomeye.py
+++ zoomeye.py
@@ -1,12 +1,37 @@
 import os 
 import requests
+import json
+
+from requests.auth import HTTPBasicAuth
+
+class zoomeye():
+
+	def __init__(self):
+		url = "https://api.zoomeye.org/user/login"
+		data = {}
+		data['username'] = 'youngcraft@qq.com'
+		data['password'] = 'cnxc2008ymgk'
+		r = requests.post(url, json=data)
+		#print r.text
+		token = json.loads(r.text)
+		#print token[u'access_token']
+		token_ring = 'JWT ' + token[u'access_token']
+		self.token = token_ring
+		self.url = url
+
+	def main(self):
+		# 100 page for use
+		for p_item in range(1,10000):
+			url = 'https://api.zoomeye.org/host/search?query=port:80 p=%d' % p_item
+			r = requests.get(url,headers = {'Authorization':self.token})
+			#print r.content
+			content = json.loads(r.content)
+			print content[u'matches'][0][u'ip']
 
 
-def zoomeye():
-	url = 
-	pass
-
-zoomeye() 
+if __name__ == '__main__':
+	x = zoomeye()
+	x.main()
 
 
 
